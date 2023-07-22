bf65889adf1b351c87b69ed86fc0180f656c196a
Vadym <vados199368@gmail.com>
2023-07-22 18:44:32 +0300

    mini fix magic numbers

--- src/helps/numbers.ts
+++ src/helps/numbers.ts
@@ -1,14 +1,20 @@
+// magic numbers
+const AMOUNT_NUMBERS = 2;
+const AMOUNT_TIME = 60;
+const MILLISECONDS = 1000;
+const AMOUNT_IN_TIME = 3600;
+
 export const getRandomNumber = (min: number, max: number): number =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
 export const formatTime = (milliseconds: number) => {
-  const totalSeconds = Math.floor(milliseconds / 1000);
-  const hours = Math.floor(totalSeconds / 3600);
-  const minutes = Math.floor((totalSeconds % 3600) / 60);
-  const seconds = totalSeconds % 60;
+  const totalSeconds = Math.floor(milliseconds / MILLISECONDS);
+  const hours = Math.floor(totalSeconds / AMOUNT_IN_TIME);
+  const minutes = Math.floor((totalSeconds % AMOUNT_IN_TIME) / AMOUNT_TIME);
+  const seconds = totalSeconds % AMOUNT_TIME;
 
-  const formattedTime = `${hours.toString().padStart(2, "0")}:${minutes
+  const formattedTime = `${hours.toString().padStart(AMOUNT_NUMBERS, '0')}:${minutes
     .toString()
-    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    .padStart(AMOUNT_NUMBERS, '0')}:${seconds.toString().padStart(AMOUNT_NUMBERS, '0')}`;
   return formattedTime;
 };
